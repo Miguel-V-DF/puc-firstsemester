c5b276e5acd27b2d6671a8a1cc0cff97c1b2c1af
Miguel Vitta <miguelvittadev@gmail.com>
2024-11-06 14:04:25 -0300

    feature: almost solved the teorema-pitagoras problem

--- maratona-iniciantes/estrutura-repeticao-3/teorema-pitagoras.c
+++ maratona-iniciantes/estrutura-repeticao-3/teorema-pitagoras.c
@@ -2,40 +2,42 @@
 
 int main()
 {
-    int x, y, z;
-    scanf("%d %d %d", &x, &y, &z);
-
-    if(z > x)
-    {
-        z = x + z;
-        x = z - x;
-        z = z - x;
-    }
-    if(y > x)
-    {
-        y = x + y;
-        x = y - x;
-        y = y - x;
-    }
-
-    if(x * x == y * y + z * z)
-    {
-        for(int i = 1; i <= z;i++)
-        {
+    int x, y, z, gcd;
+    while(scanf("%d %d %d", &x, &y, &z) != EOF) {
+        gcd = 1;
+        if(z > x) {
+            z = x + z;
+            x = z - x;
+            z = z - x;
         }
-        if(mdc(x,y,z) == 1)
-        {
-            printf("tripla pitagorica primitiva\n");
+        if(y > x) {
+            y = x + y;
+            x = y - x;
+            y = y - x;
+        }
+        if(z > y) {
+            z = y + z;
+            y = z - y;
+            z = z - y;
+        }
+        if(x * x == y * y + z * z) {
+            for(int i = 2;i <= z; i++) {
+                if(x % i == 0 && y % i == 0 && z % i == 0) {
+                    gcd = i;
+                }
+            }
+            if(gcd == 1) {
+                printf("tripla pitagorica primitiva\n");
+            }
+            else
+            {
+                printf("tripla pitagorica\n");
+            }
         }
         else
         {
-            printf("tripla pitagorica\n");
+            printf("tripla\n");
         }
     }
-    else
-    {
-        printf("tripla\n");
-    }
-
     return 0;
 }
