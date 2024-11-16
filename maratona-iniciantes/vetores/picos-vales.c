ff083f37d2d4f21d591b2ffd1ed3bd683f5d765f
Miguel Vitta <miguelvittadev@gmail.com>
2024-11-15 21:35:20 -0300

    feature: redoes the code and finally solves the picos-vales problem

--- maratona-iniciantes/vetores/picos-vales.c
+++ maratona-iniciantes/vetores/picos-vales.c
@@ -3,39 +3,27 @@
 int main()
 {
     int n, valid = 1;
-    char value = '\0';
     scanf("%d", &n);
     int h[n];
-    scanf("%d", &h[0]);
-    if(n == 0 || n == 1 || n == 2) {
-        valid = 0;
-    }
-    if(n == 2 && h[0] == h[1]) {
-        valid = 0;
-    }
-    for(int i = 1; i < n; i++) {
+
+    for(int i = 0; i < n; i++) {
         scanf("%d", &h[i]);
-        if(i == 1) {
-            if(h[1] < h[0]) {
-                value = 'D';
-            }
-            else {
-                if(h[1] > h[0]) {
-                    value = 'U';
-                }
-            }
+    }
+
+    for(int i = 1; i < n; i++) {
+        if(h[i] == h[i - 1]) {
+            valid = 0;
         }
         else {
-            if((value == 'D') && ((i % 2 == 0 && h[i] <= h[i-1]) || (i % 2 != 0 && h[i] >= h[i-1]))) {
-                valid = 0;
-            }
-            else {
-                if((value == 'U') && ((i % 2 == 0 && h[i] >= h[i-1]) || (i % 2 != 0 && h[i] <= h[i-1]))) {
+            if(i > 1) {
+                if(!((h[i - 1] > h[i - 2] && h[i] < h[i - 1]) || 
+                      (h[i - 1] < h[i - 2] && h[i] > h[i - 1]))) {
                     valid = 0;
                 }
             }
         }
     }
+
     printf("%d\n", valid);
     return 0;
 }
