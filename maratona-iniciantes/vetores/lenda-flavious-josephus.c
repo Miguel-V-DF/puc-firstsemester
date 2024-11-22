d8b5b8803076b849c501b495786fd346b5793cca
Miguel Vitta <miguelvittadev@gmail.com>
2024-11-21 23:37:32 -0300

    starting the problem of Flavious Josephus

---
+++ maratona-iniciantes/vetores/lenda-flavious-josephus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int nC;
+    scanf("%d", &nC);
+    for(int i = 0; i < nC; i++) {
+        int n, k;
+        scanf("%d %d", &n, &k);
+        int numberN[n] = {0};
+
+    }
+
+    return 0;
+}
