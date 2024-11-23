4f03f7f7661eb6da69089c1e5529e61ffec8b9e2
Miguel Vitta <miguelvittadev@gmail.com>
2024-11-22 21:06:51 -0300

    solved the palindrome problem

--- lab/experiments/class-19-11-2.c
+++ lab/experiments/class-19-11-2.c
@@ -2,18 +2,21 @@
 
 int main()
 {
-    char palavra[10], length = 0, i = 0, valid = 1;
-    
+    char palavra[100];
+    int length = 0, i = 0, valid = 1;
+
     scanf("%s", palavra);
     
     while(palavra[i] != '\0') {
         length++;
         i++;
     }
-    printf("length: %d", length);
-    for(int j = 0; j < length / 2; j++) {
-        if((palavra[j] == palavra[length - j])) {
-            valid == 0;
+    printf("length: %d\n", length);
+    int half = length / 2;
+    printf("half: %d\n", half);
+    for(int j = 0; j < half; j++) {
+        if(palavra[j] != palavra[length - j - 1]) {
+            valid = 0;
         }
     }
     if(valid == 0) {
