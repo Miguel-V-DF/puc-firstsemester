c0cc97380b4c285bea2ee7a291f06dd1912d8cce
Miguel Vitta <miguelvittadev@gmail.com>
2024-12-11 11:52:24 -0300

    solved these 3 problems

---
+++ maratona-iniciantes/matrizes/abaixo-diagonal-secundaria.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    double m[12][12], s = 0;
+    char t;
+    scanf("%c", &t);
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 12; j++) {
+            scanf("%lf", &m[i][j]);
+        }   
+    }   
+    for(int i = 1; i < 12; i++) {
+        for(int j = 12 - i; j < 12; j++) {
+            s = s + m[i][j];
+        }   
+    }   
+    if(t == 'S') {
+        printf("%.1lf\n", s); 
+    }   
+    else {
+        printf("%.1lf\n", s/66);
+    }   
+
+    return 0;
+}
